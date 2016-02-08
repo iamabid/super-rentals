dd8cdd45192e893574f6cfedb693db47e764645e
Abid Hussain <abid@freshdesk.com>
2016-02-08 14:23:54 +0530

    first commit

---
+++ app/routes/index.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+var rentals = [{
+  id: 1,
+  title: 'Grand Old Mansion',
+  owner: 'Veruca Salt',
+  city: 'San Francisco',
+  type: 'Estate',
+  bedrooms: 15,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+}, {
+  id: 2,
+  title: 'Urban Living',
+  owner: 'Mike TV',
+  city: 'Seattle',
+  type: 'Condo',
+  bedrooms: 1,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+}, {
+  id: 3,
+  title: 'Downtown Charm',
+  owner: 'Violet Beauregarde',
+  city: 'Portland',
+  type: 'Apartment',
+  bedrooms: 3,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+}];
+    if(request.queryParams.city !== undefined) {
+      let filteredRentals = rentals.filter(function(i) {
+        return i.attributes.city.toLowerCase().indexOf(request.queryParams.city.toLowerCase()) !== -1;
+      });
+      return { data: filteredRentals };
+    }
+    else {
+      return { data: rentals };
+    }
+
+export default Ember.Route.extend({
+  model() {
+
+    return rentals;
+  },
+});
